00955b50bbe1f8bc7639c98e27567abbca55e8ff
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-02-12 15:36:29 +0000

    Adjust font size

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "UbuntuMono Nerd Font Mono:size=18:antialias=true:autohint=true";
+static char *font = "UbuntuMono Nerd Font Mono:size=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
