d185342ed252011b505d898efa543c34e7bc5c73
Nerijus Vilčinskas <nerijus@intelligentpositioning.com>
2023-02-13 19:00:37 +0000

    Enable noto color emoji font for st

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static char *font = "UbuntuMono Nerd Font Mono:size=16:antialias=true:autohint=t
 static char *font2[] = {
     "UbuntuMono Nerd Font Mono:size=16:antialias=true:autohint=true",
     //"Symbola:pixelsize=15:antialias=true:autohint=true", // Unicode, works fine
-    //"Noto Color Emoji:pixelsize=15:antialias=true:autohint=true" // Coloured emoji fonts need libXft-bgra
+    "Noto Color Emoji:pixelsize=15:antialias=true:autohint=true" // Coloured emoji fonts need libXft-bgra
     //"feather" // Icomoon feather
 };
 
